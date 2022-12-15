5e3c4e2f6de9ea87607a0445b6ef34baa0b3f106
Andrey Petrov <petrovaa71@mail.ru>
2022-12-16 00:32:37 +0300

    добавил фрагмент

--- app/src/main/java/com/example/qualificationvk/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/qualificationvk/ui/activity/MainActivity.kt
@@ -3,10 +3,19 @@ package com.example.qualificationvk.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.qualificationvk.R
+import com.example.qualificationvk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
 }
--- app/src/main/java/com/example/qualificationvk/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/qualificationvk/ui/activity/SplashActivity.kt
@@ -1,6 +1,29 @@
 package com.example.qualificationvk.ui.activity
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.qualificationvk.databinding.ActivitySplashBinding
+import kotlinx.coroutines.delay
 
 class SplashActivity: AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        lifecycleScope.launchWhenCreated {
+            delay(3000)
+
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
